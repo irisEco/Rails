fbe2b8bd37c47f3b847474bb78305b52df83791e
irisEco <549104586@qq.com>
2020-07-28 12:49:45 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
+  get 'static_pages/about'
   get 'static_pages/help'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static_pages#home'
 end
