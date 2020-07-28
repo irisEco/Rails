64052c5c423d1ed599a981f18ddbd88beb7e9e5e
irisEco <549104586@qq.com>
2020-07-28 22:55:51 +0800

    ignore the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-  # has_many :microposts
-  # validates :email,presence: true
-  # validates :name,presence: true
-  attr_accessor :name,:email
-
-  def initialize(attributes={})
-    @name = attributes[:name]
-    @email = attributes[:email]
-  end
-  def formatted_email
-    "#{@name}<#{@email}>"
-  end
+  has_many :microposts
+  validates :email,presence: true
+  validates :name,presence: true
+  # attr_accessor :name,:email
+  #
+  # def initialize(attributes={})
+  #   @name = attributes[:name]
+  #   @email = attributes[:email]
+  # end
+  # def formatted_email
+  #   "#{@name}<#{@email}>"
+  # end
 end
